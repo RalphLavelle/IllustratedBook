4311603c0cce9fccd8d5cf98ff6b88e96f6589cc
Ralph Lavelle <ralphlavelle@gmail.com>
2025-06-27 08:19:25 +1000

    Chapters and Pages

--- IllustratedBook/Models/Section.cs
+++ IllustratedBook/Models/Section.cs
@@ -5,9 +5,9 @@ namespace IllustratedBook.Models
     public class Section
     {
         public int SectionId { get; set; }
-        public string Title { get; set; }
+        public string Title { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
-        public Book Book { get; set; }
+        public Book Book { get; set; } = new Book();
         public int BookId { get; set; }
         public int ParentId { get; set; }
         public string? Content { get; set; }
